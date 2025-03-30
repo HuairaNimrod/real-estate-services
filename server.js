3a18eb639a5e8eaff68528e60744349b5103a60d
Nim Huaira <nimax29@gmail.com>
2025-03-29 20:23:08 -0600

    controllers and routes

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ var express =  require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-const routes = require('express').Router();
-
-//defining the route
-routes.get('/', (req, res, next) => {
-    res.json('Hello World');
-});
-
 //attach the route to the app
-app.use('/',routes);
+app.use('/',require('./routes'));
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
